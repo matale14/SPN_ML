47bb70e0df18c4040d6ba2ae04e29e99ab0b0d16
Alex <alexmackzie@hotmail.com>
2018-11-15 11:18:10 +0100

    Filtering algorithm pretty much complete, fine tuning?

--- PCAI.py
+++ PCAI.py
@@ -4,13 +4,12 @@ import matplotlib.pyplot as plt
 import cv2
 from skimage.restoration import estimate_sigma
 import os
+from PIL import Image, ImageEnhance 
 
-
-def cai_filter(image):
+def cai_filter(image, h, w):
 
     cai_image = np.zeros_like(image) # create an empty image with the same dimensions
-    h = image.shape[0]               # Find out heigh x width for the loop
-    w = image.shape[1]
+
     print(h, "x", w)
     print(h*w, "pixels")
     size = h*w
@@ -20,10 +19,8 @@ def cai_filter(image):
     i = 0
     j = 0
 
-    h = int(h)
-    w = int(w)
-    for y in range(0, 500):         # for all pixels in y axis
-        for x in range(0, 500):     # for all pixels in x axis
+    for y in range(0, h):         # for all pixels in y axis For some reason, I have to specify the amounts
+        for x in range(0, w):     # for all pixels in x axis The variables above changes for some reason
 
             progress = round((j / size) * 100, 1)
             progressbar = int(progress / 4)
@@ -33,26 +30,26 @@ def cai_filter(image):
                 n = image[y - 1, x] # North, east, south, west pixels
                 e = image[y, x + 1]
                 s = image[y + 1, x]
-                w = image[y, x - 1]
+                we = image[y, x - 1]
             except IndexError:
                 n = image[y, x]
                 e = image[y, x]
                 s = image[y, x]
-                w = image[y, x]
+                we = image[y, x]
 
-            cai_array = [n, e, s, w]
+            cai_array = [n, e, s, we]
 
             n = int(n)
             e = int(e)
             s = int(s)
-            w = int(w)
+            we = int(we)
 
             if (np.max(cai_array) - np.min(cai_array)) <= 20:       # If the max number of the neighbouring pixels are less than or equal to
                 px = np.mean(cai_array)                             # 20 in value(0-255) then just set the pixel to the mean
-            elif (np.absolute(e - w) - np.absolute(n - s)) > 20:    # If the absolute value(not negative. F.ex. -5 = 5) of that is more than 20
+            elif (np.absolute(e - we) - np.absolute(n - s)) > 20:    # If the absolute value(not negative. F.ex. -5 = 5) of that is more than 20
                 px = (n + s) / 2                                    # the value is northern + southern pixel divided by 2
-            elif (np.absolute(n - s) - np.absolute(e - w)) > 20:
-                px = (e + w) / 2
+            elif (np.absolute(n - s) - np.absolute(e - we)) > 20:
+                px = (e + we) / 2
             else:
                 px = np.median(cai_array)                           # Median is backup. Median just selects the item that is most in the middle.
 
@@ -85,80 +82,57 @@ def calc_sigma(image):
     d = image
     m = 3
     sigma_0 = 9
-    sigmage = np.zeros_like(d)
-    h = d.shape[0]               # Find out heigh x width for the loop
-    w = d.shape[1]
-    size = h*w
 
-    print("Calculating sigma image")
-    i = 0
-    j = 0
+    #Wu et al.
+    #http://ws.binghamton.edu/fridrich/Research/double.pdf
+    #m = the neighbourhood pixels, here the 3x3 pixels around the selected pixel
+    #sum the value of the neighbourhood - the overall variance of the SPN.
 
-    h = int(h)
-    w = int(w)
-    
-    for y in range(0, 500):         # for all pixels in y axis
-        for x in range(0, 500):     # for all pixels in x axis
+    sigsum = ((1/m**2)* np.sum((d**2) -(float(sigma_0))))      
 
-            progress = round((j / size) * 100, 1)
-            progressbar = int(progress / 4)
-            print('\r|{}|{}%'.format(("█" * progressbar), progress), end="", flush=True)
+    sigmas=(0, sigsum)
+    local_variance = max(sigmas)
 
-            #Sigsum here. Need to read up more on it from source[3] in Wu et al.
-            #http://ws.binghamton.edu/fridrich/Research/double.pdf
+    return local_variance
 
-            #for now using est_sigma() which uses estimate_sigma() from skimage
-            #that uses D. L. Donoho and I. M. Johnstone. “Ideal spatial adaptation by wavelet shrinkage.” 
-            #Biometrika 81.3 (1994): 425-455. DOI:10.1093/biomet/81.3.425
-            #http://scikit-image.org/docs/dev/api/skimage.restoration.html#skimage.restoration.estimate_sigma
 
-            sigmas=(0, sigsum)
-            px = max(sigmas)
-            sigmage[y, x] = px
-
-    return sigmage
-
-def est_sigma(image):
-    dimage = image
-    sigma_est = estimate_sigma(dimage, multichannel=True, average_sigmas=True)
-    return sigma_est
-
-def wavelet(dimage, sigmage):
+def wavelet(dimage, h, w):
     d = dimage
-    sigma = sigmage
     sigma_0 = 9
 
     wav_image = np.zeros_like(d) # create an empty image with the same dimensions
-    h = d.shape[0]               # Find out heigh x width for the loop
-    w = d.shape[1]
+
     size = h*w
 
     print("Wavelet transform")
-    i = 0
     j = 0
 
-    h = int(h)
-    w = int(w)
     
-    for y in range(0, 500):         # for all pixels in y axis
-        for x in range(0, 500):     # for all pixels in x axis
+    for y in range(0, h-3):         # for all pixels in y axis
+        for x in range(0, w-3):     # for all pixels in x axis
 
             progress = round((j / size) * 100, 1)
             progressbar = int(progress / 4)
             print('\r|{}|{}%'.format(("█" * progressbar), progress), end="", flush=True)
-            
-            d_px = d[y, x]          
-            sigma_div = sigma_0/(sigma + sigma_0) # According to the formulas in Wu et al.
-            px = d_px * sigma_div                       # link here later(Or check wiki)
-            wav_image[y, x] = px
 
+
+                                                                    # According to the formulas in Wu et al.
+            d_px = d[y, x]                                          # Select current pixel
+            local_area = d[range(y-3, y+3), range(x-3,x+3)]         # Select the 9 pixels around it from the CAI subtraction
+            sigma_div = sigma_0/(calc_sigma(local_area) + sigma_0)  # get the estimated local variance for the pixel
+            px = d_px * sigma_div                                   # multiply subtracted CAI with the local variances
+            px= int(px)                                             # Estimated camera reference SPN
+            wav_image[y, x] = px
+            j += 1
+
+    print('\r|{}|{}%'.format(("█" * 25), "100.0"), end="", flush=True)
+    print()
     return wav_image
 
 def get_spn(wav_image):
 
-    average = wav_image[wav_image!=0].mean()    #average all the noise and add them
-    spn = average + wav_image
-    return spn
+    average = wav_image[wav_image!=0].mean()    #average all the noise and add them 
+    return average
 
 
 def crop_center(img, cropx, cropy):
@@ -180,31 +154,38 @@ def crop_center(img, cropx, cropy):
         starty = y // 2 - cropy // 2
         return img[starty:starty + cropy, startx:startx + cropx]
 
+
 if __name__ == "__main__":
     img_path = 'example.jpg'
+    
+    w = 512
+    h = 512
+
     if os.path.isfile(img_path):
         original = cv2.imread(img_path, 0)                  # the 0 means read as grayscale
         plt.imsave('0_Original.png', original, cmap="gray") # cmap="gray" means save as grayscale
 
-        cropped = crop_center(original, 500, 500)
+        cropped = crop_center(original, h, w)
         plt.imsave('1_Cropped.png', cropped, cmap="gray") 
 
-        cai = cai_filter(cropped)
-        plt.imsave('2_CAI.png', cai, cmap="gray")
+        cai_image = cai_filter(cropped, h, w)
+        plt.imsave('2_CAI.png', cai_image, cmap="gray")
 
-        d_image = cv2.subtract(cai, cropped)
+        d_image = cv2.subtract(cai_image, cropped)
         plt.imsave('3_D_image.png', d_image, cmap="gray")
 
-        sig_image = est_sigma(d_image)
-        #plt.imsave('4_Sig_image.png', sig_image, cmap="gray")
+        wav_image = wavelet(d_image, h, w)
+        plt.imsave('4_Wav_image.png', wav_image, cmap="gray")
 
-        #sig_image = calc_sigma(d_image)
-        #plt.imsave('4_Sig_image.png', sig_image, cmap="gray")
+        img = Image.fromarray(wav_image)
+        enhancer = ImageEnhance.Brightness(img)
+        enhanced_im = enhancer.enhance(1.8)
+        enhanced_im.save('5_brighten.png')
+        #plt.imsave('5_brighten.png', enhanced_im, cmap="gray")
 
-        wav_image = wavelet(sig_image, d_image)
-        plt.imsave('5_Wav_image.png', wav_image, cmap="gray")
 
-        spn_image = get_spn(wav_image, d_image)
-        plt.imsave('6_SPN_image.png', spn_image, cmap="gray")
+
+        spn = get_spn(wav_image)
+        print("Sensor Pattern Noise reference number:", spn)
     else:
         print("file does not exist")
