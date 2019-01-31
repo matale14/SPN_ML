d0498f0c4605dd8fc46330e405e76be1f065e62a
Bjarke Larsen <bjarke.larsen@gmail.com>
2019-01-31 12:22:44 +0100

    Making progress

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,31 +1,132 @@
+from glob import glob
+from os.path import join, basename
+from os import mkdir
+from PIL import Image
+from sys import platform
 from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.gridlayout import GridLayout
-from glob import glob
-from os.path import join
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scrollview import ScrollView
+
+
+
 """
 Created by: Bjarke Larsen
+
+Note: I had to import glob, os, PIL and sys before all kivy modules, otherwise the photos didn't load. Haven't 
+tested if it affects the creation of the thumbnails.
 """
 
-class TestApp(App):
-
+class SPAI(App):
     def build(self):
-        layout = GridLayout(cols=3)
+        #Creating the layout
+        root = FloatLayout()
+        scroll = ScrollView(pos_hint={"x": 0.2, "y": 0.0})
+
+        #Adding the layouts together
+        root.add_widget(self._sidepanel())
+        root.add_widget(scroll)
+        scroll.add_widget(self._showphotos())
+
+
+        return root
+
+
+    def _sidepanel(self):
+        layout = BoxLayout(orientation="vertical", size_hint=(None, 1), pos_hint={"x": 0.0, "y": 0.0})
+        layout.add_widget(Button(text="Import Photos", size_hint=(1,0.2)))
+        layout.add_widget(Button(text="Create Report", size_hint=(1,0.2)))
+        layout.add_widget(Button(text="Save?", size_hint=(1,0.2)))
+        layout.add_widget(Button(text="Button 4", size_hint=(1,0.2)))
+        return layout
+
+    def _showphotos(self):
+        layout = BoxLayout(orientation="vertical", padding=20, spacing=30, size_hint_y=None)
+        layout.bind(minimum_height=layout.setter('height'))
         #Specifies the path of the main folder, the first * in the "glob(join(curdir, '*', '*'))" is the subfolder
         # containing the images. An * is used to import from all subfolders, if there are several. The last * is the
         # image name, again * is used as a wild card.
+
         curdir = "/Users/Bjarke/Desktop/Test/"
-        for filename in glob(join(curdir, '*', '*')):
+
+        """Note for my self. Try implement the button and presentation in a FloatLayout, then it can be more smooth."""
+
+        for folders in glob(join(curdir, "*")):
             try:
-                # load the image
-                im = Image(source=filename)
-                layout.add_widget(im)
+                name = str(folders)
+                btn = Button(text=name, size=(100, 100))
+                layout.add_widget(btn)
+                presentation = GridLayout(cols=20, padding=0, spacing=10, size_hint_y=None)
 
-            except Exception:
-                print("Pictures: Unable to load <%s>" % filename)
+                for filename in glob(join(curdir, folders, "*")):
+                    try:
+                        # load the image
+                        im = Image(source=filename)
+                        presentation.add_widget(im)
 
+                    except Exception:
+                        print("Pictures: Unable to load <%s>" % filename)
+
+                layout.add_widget(presentation)
+
+            except:
+                pass
 
         return layout
 
+    def _create_thumbs(self):
+        curdir = "/Users/Bjarke/Desktop/Test/"
+        try:
+            mkdir(curdir + "thump")
+        except FileExistsError:
+            pass
+        size = 128, 128
+        if platform == "darwin" or platform == "linux":
+            for folder in glob(join(curdir, "*")):
+                folder_name = basename(folder)
+                if folder_name == "thump":
+                    pass
+                else:
+                    thumbfolder_path = curdir + "thump/" + folder_name
+                    try:
+                        mkdir(thumbfolder_path)
+
+                    except FileExistsError:
+                        pass
+
+                    try:
+                        for picture in glob(join(curdir, folder, "*")):
+                            picture_name = basename(picture)
+                            im = Image.open(picture)
+                            im.thumbnail(size)
+                            im.save(thumbfolder_path + "/" + picture_name, "JPEG")
+                    except:
+                        pass
+        else:
+            for folder in glob(join(curdir, "*")):
+                folder_name = basename(folder)
+                if folder_name == "thump":
+                    pass
+                else:
+                    thumbfolder_path = str(curdir + r"'thump\'" + folder_name)
+                    try:
+                        mkdir(thumbfolder_path)
+
+                    except FileExistsError:
+                        pass
+
+                    try:
+                        for picture in glob(join(curdir, folder, "*")):
+                            picture_name = basename(picture)
+                            im = Image.open(picture)
+                            im.thumbnail(size)
+                            im.save(thumbfolder_path + r"'\'" + picture_name, "JPEG")
+                    except:
+                        pass
+
+
 if __name__ == "__main__":
-    TestApp().run()
+    SPAI().run()
